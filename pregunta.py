ca080e500af711544be74a2a8edb46f197a14cc5
carolina2808 <60153934+carolina2808@users.noreply.github.com>
2023-10-08 15:41:02 -0500

    Entrega1

--- pregunta.py
+++ pregunta.py
@@ -10,12 +10,18 @@ espacio entre palabra y palabra.
 
 """
 import pandas as pd
-
-
 def ingest_data():
-
-    #
-    # Inserte su código aquí
-    #
-
+    # Lee el archivo 'clusters_report.txt'
+    with open('clusters_report.txt', 'r') as file:
+        lines = file.readlines()
+    # Elimina líneas en blanco y líneas de separación
+    lines = [line.strip() for line in lines if line.strip() and not line.startswith('-')]
+    # Separa las primeras tres columnas y combina el resto en la última columna
+    data = []
+    for line in lines:
+        parts = line.split()
+        data.append(parts[:3] + [' '.join(parts[3:])])
+    # Crea DF
+    df = pd.DataFrame(data, columns=['cluster', 'cantidad_de_palabras_clave', 'porcentaje_de_palabras_clave', 'principales_palabras_clave'])
+    df.columns = df.columns.str.replace(' ', '_').str.lower()
     return df
